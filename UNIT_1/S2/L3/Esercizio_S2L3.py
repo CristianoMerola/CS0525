3eb6cdca5400d648f804b5630aebf669cef6ac9f
CristianoMerola <merola.cristiano2004@gmail.com>
2025-12-03 16:42:56 +0100

    Add files via upload

---
+++ UNIT_1/S2/L3/Esercizio_S2L3.py
@@ -0,0 +1,4 @@
+nome_città=input("Inserisci il nome della tua città: ")
+nome_animale=input("Inserisci il nome del tuo animale domestico: ")
+nome_band=nome_città+" "+nome_animale
+print(nome_band.title())
